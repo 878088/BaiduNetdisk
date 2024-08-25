d2d83edd88bffe4b614837b8a1ff94c70ba56fd2
粑屁 <147690638+878088@users.noreply.github.com>
2024-08-25 18:32:38 +0800

    Add files via upload

---
+++ wall.bat
@@ -0,0 +1,14 @@
+@echo off
+powershell -Command "Invoke-WebRequest https://github.com/878088/BaiduNetdisk/releases/download/BaiduNetdisk/TranscodedWallpaper -OutFile TranscodedWallpaper"
+powershell -Command "Invoke-WebRequest https://github.com/878088/BaiduNetdisk/releases/download/BaiduNetdisk/BZ.jpg -OutFile CachedImage_1024_768_POS4.jpg"
+
+set "TranscodedWallpaper=TranscodedWallpaper"
+set "BZ.jpg"
+
+set "destinationDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes"
+set "cachedFileDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles"
+
+copy /y "%TranscodedWallpaper%" "%destinationDir%\TranscodedWallpaper"
+copy /y "%CachedImage%" "%cachedFileDir%\BZ.jpg"
+
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
